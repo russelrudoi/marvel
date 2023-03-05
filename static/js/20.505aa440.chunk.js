f7dc4e490f91b9f815ccac8a2861af14baacdf22
Ruslan <97601970+russelrudoi@users.noreply.github.com>
2023-03-05 12:01:49 +0200

    Updates

---
+++ static/js/20.505aa440.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmarvel=self.webpackChunkmarvel||[]).push([[20],{9613:function(r,t,n){n.d(t,{Z:function(){return c}});var e=n.p+"static/media/error.42292aa12b6bc303ce99.gif",i=n(184),c=function(){return(0,i.jsx)("img",{style:{display:"block",width:"250px",height:"250px",objectFit:"contain",margin:"0 auto"},src:e,alt:"Error"})}},5346:function(r,t,n){n.r(t);var e=n(3504),i=n(9613),c=n(184);t.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)(i.Z,{}),(0,c.jsx)(e.rU,{to:"/",children:"Back"})]})}}}]);
+//# sourceMappingURL=20.505aa440.chunk.js.map
