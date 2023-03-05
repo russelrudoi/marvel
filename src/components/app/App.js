f8b38a3795c3a956657f62548328f3a3d1a6a6f6
Ruslan <97601970+russelrudoi@users.noreply.github.com>
2023-03-05 14:12:08 +0200

    Update App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -19,17 +19,17 @@ const App = () => {
                     <main>
                         <Suspense fallback={<span>Loading</span>}>
                             <Routes>
-                                <Route path="/marvel/" element={<MainPage/>} />
-                                <Route path="/marvel/comics" element={<ComicsPage/>} />
+                                <Route path="/" element={<MainPage/>} />
+                                <Route path="/comics" element={<ComicsPage/>} />
                                 <Route path="*" element={<Page404/>} />
                                 <Route 
-                                    path="/marvel/comics/:id" 
+                                    path="/comics/:id" 
                                     element={<SinglePage 
                                         Component={SingleComicLayout} 
                                         dataType ='comic' />} 
                                 />
                                 <Route 
-                                    path="/marvel/characters/:id" 
+                                    path="/characters/:id" 
                                     element={<SinglePage 
                                             Component={SingleCharacterLayout} 
                                             dataType ='character' />}
